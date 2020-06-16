0c69f1b6802ace251637cde136a9ce07e9b68402
riaz.shaik <riaz.shaik@CPX-Z9QOOTV9YHT.dir.svc.accenture.com>
2020-06-16 12:38:50 +0100

    update

--- src/main/java/io/javabrains/SpringDiscoveryServerApplication.java
+++ src/main/java/io/javabrains/SpringDiscoveryServerApplication.java
@@ -2,16 +2,24 @@ package io.javabrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 @EnableEurekaServer
 @RefreshScope
-public class SpringDiscoveryServerApplication {
+public class SpringDiscoveryServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDiscoveryServerApplication.class, args);
 	}
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringDiscoveryServerApplication.class); 
+	} 
 
 }
